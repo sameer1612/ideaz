4f3bb5d6860820f122f3397e911dd67fb358394f
Sameer Kumar <kumar.sam1612@gmail.com>
2022-09-13 18:53:08 +0530

    Revert "added ng material"
    This reverts commit a3d583b2e4a7c955cc448040c8626cf0c8a29ffb.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
